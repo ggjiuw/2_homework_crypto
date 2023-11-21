8610890db245f364d14db122a18a32d5b5f63d5a
ggjiuw <herezoom1231@gmail.com>
2023-11-21 13:46:03 +0200

    main file

--- jwt_library.py
+++ jwt_library.py
@@ -1,13 +1,12 @@
 import jwt
 import settings
 
+
 def decode(data: str) -> dict:
     decoded_result = jwt.decode(
         data,
         settings.JWT_SECRET,
-        algorithms=['HS256'],
-        options={
-        'verify_signature': False
-        }
+        algorithms=["HS256"],
+        options={"verify_signature": False},
     )
     return decoded_result
