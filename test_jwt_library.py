031f59c83b892f4bcd41871bf79894d78f9289e8
ggjiuw <herezoom1231@gmail.com>
2023-11-21 13:46:21 +0200

    for pytest

--- test_jwt_library.py
+++ test_jwt_library.py
@@ -1,11 +1,8 @@
 import jwt_library
 
+
 def test_decode():
-    data = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZ2UiOi03LCJuYW1lIjoiZ2dqaXV3IiwiaWF0IjoxNTE2MjM5MDIyfQ.JtY-vs16O3hABvQYXS9BScOH7iiZxpT6PQ333jE0WM4'
-    expected_result = {
-        "age": -7,
-        "name": "ggjiuw",
-        "iat": 1516239022
-    }
+    data = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZ2UiOi03LCJuYW1lIjoiZ2dqaXV3IiwiaWF0IjoxNTE2MjM5MDIyfQ.JtY-vs16O3hABvQYXS9BScOH7iiZxpT6PQ333jE0WM4"
+    expected_result = {"age": -7, "name": "ggjiuw", "iat": 1516239022}
     actual_result = jwt_library.decode(data=data)
     assert expected_result == actual_result
